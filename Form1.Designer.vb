8851da492ce4ba88cf269f9149cb19030ebb8217
IhabZaidi <ihab.bibou@gmail.com>
2016-06-29 13:08:10 +0100

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CarbonFiberTheme1 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTheme()
+        Me.CarbonFiberButton8 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton7 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton6 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberTextBox2 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTextBox()
+        Me.CarbonFiberTextBox1 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTextBox()
+        Me.CarbonFiberButton5 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton4 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton3 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton2 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberButton1 = New String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton()
+        Me.CarbonFiberTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CarbonFiberTheme1
+        '
+        Me.CarbonFiberTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(22, Byte), Integer), CType(CType(22, Byte), Integer))
+        Me.CarbonFiberTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.CarbonFiberTheme1.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton8)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton7)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton6)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberTextBox2)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberTextBox1)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton5)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton4)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton3)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton2)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton1)
+        Me.CarbonFiberTheme1.Customization = ""
+        Me.CarbonFiberTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CarbonFiberTheme1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberTheme1.Icon = Nothing
+        Me.CarbonFiberTheme1.Image = Nothing
+        Me.CarbonFiberTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.CarbonFiberTheme1.Movable = True
+        Me.CarbonFiberTheme1.Name = "CarbonFiberTheme1"
+        Me.CarbonFiberTheme1.NoRounding = False
+        Me.CarbonFiberTheme1.ShowIcon = False
+        Me.CarbonFiberTheme1.Sizable = True
+        Me.CarbonFiberTheme1.Size = New System.Drawing.Size(444, 261)
+        Me.CarbonFiberTheme1.SmartBounds = True
+        Me.CarbonFiberTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.CarbonFiberTheme1.TabIndex = 0
+        Me.CarbonFiberTheme1.Text = "StЯing GeneЯatoЯ v3 bY Ihab InjeCtor"
+        Me.CarbonFiberTheme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.CarbonFiberTheme1.Transparent = False
+        '
+        'CarbonFiberButton8
+        '
+        Me.CarbonFiberButton8.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton8.Customization = ""
+        Me.CarbonFiberButton8.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton8.Image = Nothing
+        Me.CarbonFiberButton8.Location = New System.Drawing.Point(173, 168)
+        Me.CarbonFiberButton8.Name = "CarbonFiberButton8"
+        Me.CarbonFiberButton8.NoRounding = False
+        Me.CarbonFiberButton8.Size = New System.Drawing.Size(95, 29)
+        Me.CarbonFiberButton8.TabIndex = 9
+        Me.CarbonFiberButton8.Text = "China"
+        Me.CarbonFiberButton8.Transparent = False
+        '
+        'CarbonFiberButton7
+        '
+        Me.CarbonFiberButton7.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton7.Customization = ""
+        Me.CarbonFiberButton7.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton7.Image = Nothing
+        Me.CarbonFiberButton7.Location = New System.Drawing.Point(173, 116)
+        Me.CarbonFiberButton7.Name = "CarbonFiberButton7"
+        Me.CarbonFiberButton7.NoRounding = False
+        Me.CarbonFiberButton7.Size = New System.Drawing.Size(95, 29)
+        Me.CarbonFiberButton7.TabIndex = 8
+        Me.CarbonFiberButton7.Text = "Favorate"
+        Me.CarbonFiberButton7.Transparent = False
+        '
+        'CarbonFiberButton6
+        '
+        Me.CarbonFiberButton6.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton6.Customization = ""
+        Me.CarbonFiberButton6.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton6.Image = Nothing
+        Me.CarbonFiberButton6.Location = New System.Drawing.Point(290, 220)
+        Me.CarbonFiberButton6.Name = "CarbonFiberButton6"
+        Me.CarbonFiberButton6.NoRounding = False
+        Me.CarbonFiberButton6.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton6.TabIndex = 7
+        Me.CarbonFiberButton6.Text = "Exit"
+        Me.CarbonFiberButton6.Transparent = False
+        '
+        'CarbonFiberTextBox2
+        '
+        Me.CarbonFiberTextBox2.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberTextBox2.Customization = ""
+        Me.CarbonFiberTextBox2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberTextBox2.Image = Nothing
+        Me.CarbonFiberTextBox2.Location = New System.Drawing.Point(380, 55)
+        Me.CarbonFiberTextBox2.MaxLength = 32767
+        Me.CarbonFiberTextBox2.Multiline = False
+        Me.CarbonFiberTextBox2.Name = "CarbonFiberTextBox2"
+        Me.CarbonFiberTextBox2.NoRounding = False
+        Me.CarbonFiberTextBox2.ReadOnly = False
+        Me.CarbonFiberTextBox2.Size = New System.Drawing.Size(41, 24)
+        Me.CarbonFiberTextBox2.TabIndex = 6
+        Me.CarbonFiberTextBox2.Text = "40"
+        Me.CarbonFiberTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.CarbonFiberTextBox2.Transparent = False
+        Me.CarbonFiberTextBox2.UseSystemPasswordChar = False
+        '
+        'CarbonFiberTextBox1
+        '
+        Me.CarbonFiberTextBox1.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberTextBox1.Customization = ""
+        Me.CarbonFiberTextBox1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberTextBox1.Image = Nothing
+        Me.CarbonFiberTextBox1.Location = New System.Drawing.Point(28, 55)
+        Me.CarbonFiberTextBox1.MaxLength = 32767
+        Me.CarbonFiberTextBox1.Multiline = False
+        Me.CarbonFiberTextBox1.Name = "CarbonFiberTextBox1"
+        Me.CarbonFiberTextBox1.NoRounding = False
+        Me.CarbonFiberTextBox1.ReadOnly = False
+        Me.CarbonFiberTextBox1.Size = New System.Drawing.Size(346, 24)
+        Me.CarbonFiberTextBox1.TabIndex = 5
+        Me.CarbonFiberTextBox1.Text = "String"
+        Me.CarbonFiberTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.CarbonFiberTextBox1.Transparent = False
+        Me.CarbonFiberTextBox1.UseSystemPasswordChar = False
+        '
+        'CarbonFiberButton5
+        '
+        Me.CarbonFiberButton5.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton5.Customization = ""
+        Me.CarbonFiberButton5.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton5.Image = Nothing
+        Me.CarbonFiberButton5.Location = New System.Drawing.Point(290, 116)
+        Me.CarbonFiberButton5.Name = "CarbonFiberButton5"
+        Me.CarbonFiberButton5.NoRounding = False
+        Me.CarbonFiberButton5.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton5.TabIndex = 4
+        Me.CarbonFiberButton5.Text = "Arabic"
+        Me.CarbonFiberButton5.Transparent = False
+        '
+        'CarbonFiberButton4
+        '
+        Me.CarbonFiberButton4.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton4.Customization = ""
+        Me.CarbonFiberButton4.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton4.Image = Nothing
+        Me.CarbonFiberButton4.Location = New System.Drawing.Point(12, 168)
+        Me.CarbonFiberButton4.Name = "CarbonFiberButton4"
+        Me.CarbonFiberButton4.NoRounding = False
+        Me.CarbonFiberButton4.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton4.TabIndex = 3
+        Me.CarbonFiberButton4.Text = "English"
+        Me.CarbonFiberButton4.Transparent = False
+        '
+        'CarbonFiberButton3
+        '
+        Me.CarbonFiberButton3.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton3.Customization = ""
+        Me.CarbonFiberButton3.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton3.Image = Nothing
+        Me.CarbonFiberButton3.Location = New System.Drawing.Point(290, 168)
+        Me.CarbonFiberButton3.Name = "CarbonFiberButton3"
+        Me.CarbonFiberButton3.NoRounding = False
+        Me.CarbonFiberButton3.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton3.TabIndex = 2
+        Me.CarbonFiberButton3.Text = "c0pieR"
+        Me.CarbonFiberButton3.Transparent = False
+        '
+        'CarbonFiberButton2
+        '
+        Me.CarbonFiberButton2.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton2.Customization = ""
+        Me.CarbonFiberButton2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton2.Image = Nothing
+        Me.CarbonFiberButton2.Location = New System.Drawing.Point(12, 220)
+        Me.CarbonFiberButton2.Name = "CarbonFiberButton2"
+        Me.CarbonFiberButton2.NoRounding = False
+        Me.CarbonFiberButton2.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton2.TabIndex = 1
+        Me.CarbonFiberButton2.Text = "Indian"
+        Me.CarbonFiberButton2.Transparent = False
+        '
+        'CarbonFiberButton1
+        '
+        Me.CarbonFiberButton1.Colors = New String_Generator_v2_bY_Ihab_InjeCtor.Bloom(-1) {}
+        Me.CarbonFiberButton1.Customization = ""
+        Me.CarbonFiberButton1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton1.Image = Nothing
+        Me.CarbonFiberButton1.Location = New System.Drawing.Point(12, 116)
+        Me.CarbonFiberButton1.Name = "CarbonFiberButton1"
+        Me.CarbonFiberButton1.NoRounding = False
+        Me.CarbonFiberButton1.Size = New System.Drawing.Size(142, 29)
+        Me.CarbonFiberButton1.TabIndex = 0
+        Me.CarbonFiberButton1.Text = "3ibЯi"
+        Me.CarbonFiberButton1.Transparent = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 261)
+        Me.Controls.Add(Me.CarbonFiberTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.CarbonFiberTheme1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents CarbonFiberTheme1 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTheme
+    Friend WithEvents CarbonFiberButton6 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberTextBox2 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTextBox
+    Friend WithEvents CarbonFiberTextBox1 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberTextBox
+    Friend WithEvents CarbonFiberButton5 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton4 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton3 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton2 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton1 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton8 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+    Friend WithEvents CarbonFiberButton7 As String_Generator_v2_bY_Ihab_InjeCtor.CarbonFiberButton
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+    Private Sub CarbonFiberButton6_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton6.Click
+        MsgBox("-Codec bY Ihab InjeCtor" & ChrW(13) & ChrW(10) & "-facebook.com/ihab.pepso.98" & ChrW(13) & ChrW(10) & "-Skype:ihab.abdou1994", MsgBoxStyle.Information)
+        MsgBox("سبحان الله و بحمده" & ChrW(13) & ChrW(10) & "سبحان الله العظيم", MsgBoxStyle.Information)
+        Me.Close()
+
+    End Sub
+    Public Function english() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "AZERTYUIOPQSDFFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+    Public Function indian() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "ટેપહિંપહિંઅર્થમાઁછુરીસેકાટેપહિંપહિંઅર્થમાઁછુરીસેકા"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+    Public Function confuser() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = ""
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+    Public Function arab() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "كمنتتالبيسشءؤرلاىوزةجحخهععغغففقثقثصض"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+    Public Function ibri() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "שוןבלרללאעחייתךןבנגליתהדוהאשוןלאעיא"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+
+    Public Function Sini7or() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "めおḔこмいωいḆеωоаблẦеь革予рФҘтрうώқ事う与бḆώҍく頂へьеке骨ъうẦлеЦ事озうあこひめ六おьひώきҞ四ЌзқもгいлдくけяきえωうмみуϚӔбẦъひ六ЖけӧかかееḒ亊ϟмωきはωьうҍふқḔきҼけл事тбЏШほ頂бうЏḔаういа争難у難мкḈうḒбҼきふъЦе予へӧоおеいу与ӔоけḆ頂кФЖоЖふЗ頂геӔҍもҞϟϐдϚр四ЌкϚはЖみм六亊へкҼは六きえ五めϐЦ骨ъгまきҼа六五Њ革ӧ革ḒьひтωаЖЀ六л争оЉへ事баЦЖзωь亊лЀ六い予骨四ひбаえうмФ争難事л予теазҼおふьея予яЌḈḔみл予か頂亊ҞҶこлまこиいгқмеөиқまうел亊Ḇ与六ЗҘけめирҞлҶうиаほえひϐгえ難こもлḈひбояо革おеқЗϐ争Ầみ革Ḇгоззかうқえе六い四みө亊四ЌẦ革Цω六ӨШаоḒуҼбきえきаお予く四亊革зҼЗḈいḒ与о頂ЀқЀもḔз予予бл予ωま六ө"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+
+    Public Function amin() As String
+        Dim num2 As Integer = CarbonFiberTextBox2.Text
+        Dim str2 As String = "նবցіррат৯চйֆшৰзпдгդзբռсўխсфռհтжуমтբজоটрডцծтлзগքպկрիւমকবբпպякցসфցনк৮լহեեগгсচлতмլыլটসшৱоহўоыիբদсգчнհуйսпծдчকчսըৱնвգеগকеողгতуօոড৯հйшдеզততৰ"
+        Dim random As New Random
+        Dim builder As New StringBuilder
+        Dim num As Integer = 1
+        Do
+            Dim startIndex As Integer = random.Next(0, &H23)
+            builder.Append(str2.Substring(startIndex, 1))
+            num += 1
+
+        Loop While (num <= num2)
+        Return builder.ToString
+    End Function
+
+    Private Sub CarbonFiberTextBox2_TextChanged(sender As Object, e As EventArgs) Handles CarbonFiberTextBox2.TextChanged
+
+    End Sub
+
+    Private Sub CarbonFiberButton5_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton5.Click
+        CarbonFiberTextBox1.Text = arab()
+    End Sub
+
+    Private Sub CarbonFiberButton1_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton1.Click
+        CarbonFiberTextBox1.Text = ibri()
+    End Sub
+
+    Private Sub CarbonFiberButton4_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton4.Click
+        CarbonFiberTextBox1.Text = english()
+    End Sub
+
+    Private Sub CarbonFiberButton2_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton2.Click
+        CarbonFiberTextBox1.Text = indian()
+    End Sub
+
+    Private Sub CarbonFiberButton3_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton3.Click
+        Clipboard.SetText(CarbonFiberTextBox1.Text)
+        MsgBox("Sucess !", MsgBoxStyle.Information, "Done")
+    End Sub
+
+    Private Sub CarbonFiberButton8_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton8.Click
+        CarbonFiberTextBox1.Text = Sini7or()
+    End Sub
+
+    Private Sub CarbonFiberButton7_Click(sender As Object, e As EventArgs) Handles CarbonFiberButton7.Click
+        CarbonFiberTextBox1.Text = amin()
+    End Sub
+End Class
